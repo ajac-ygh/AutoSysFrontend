151e4505422cca1e876c768d1800e53bba7f3869
Luigi Vismara <33238929+luigivis@users.noreply.github.com>
2023-03-15 15:04:12 -0500

    Delete autosys directory

--- autosys/src/utils/endpointCatalog.jsx
+++
@@ -1,8 +0,0 @@
-const getPathLoginByForm = () => {
-  return "http://" + process.env.REACT_APP_IP_ADDRESS + ":4000/api/v1/security/auth";
-}
-const getPathLoginByToken = () => {
-  return "http://" + process.env.REACT_APP_IP_ADDRESS + ":4000/api/v1/security/tokenValid";
-}
-
-export {getPathLoginByForm, getPathLoginByToken};
